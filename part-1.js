0b5b0f4afe72f24c8d30ba0b713bdf517f74a0e3
nataliakocol <natalia.kocol@ada.ac.uk>
2018-10-26 14:35:40 +0100

    finished 1 part

--- part-1.js
+++ part-1.js
@@ -1,1 +1,26 @@
-console.log('your code here')
+var fs = require('fs');
+var fileContents = fs.readFileSync('names.txt', 'utf-8');
+var arr = fileContents.split('\n'); //elements were seperated by new line
+
+for (var i = 0 in arr) {
+  console.log(arr[i] + ' break');
+}
+
+arr.reverse();
+//for (var i in arr)
+for (var i of arr) {
+  console.log(i + ' break');
+  name = i + '.txt';
+  arrrev = reverseString(i);
+  fs.writeFileSync(name, arrrev, 'utf-8');
+}
+
+arr = arr.join(', '); //join the elements into string, they will be sepereted by ", "
+console.log(arr);
+
+//fs.writeFileSync(nameOfFile, fileContents, fileEncoding)
+fs.writeFileSync('names-reversed.txt', arr, 'utf-8');
+
+function reverseString(str) {
+    return str.split( '' ).reverse( ).join( '' );
+}
